2327433548c08a4ad1b41d718ea39efbf1af661e
Eman Ahmed <eman455sallam@gmail.com>
2026-05-01 00:07:45 +0300

    update diploma details exams layout

--- src/app/features/diplomas/components/diploma-details/diploma-details.ts
+++ src/app/features/diplomas/components/diploma-details/diploma-details.ts
@@ -1,23 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DiplomasService } from '../../services/diplomas-service';
-import { DiplomaDetailsModel } from '../../interfaces/diploma-details-response';
+import { DiplomaDetailsModel, Exam } from '../../interfaces/diploma-details-response';
 import { Breadcrumbs } from "../../../../shared/components/breadcrumbs/breadcrumbs";
 import { BreadcrumbService } from '../../../../shared/services/breadcrumb.service';
 import { PageTitle } from "../../../../shared/components/page-title/page-title";
 import { BackArrow } from "../../../../shared/components/back-arrow/back-arrow";
-import { BookOpenCheck } from 'lucide-angular';
+import { BookOpenCheck, CircleQuestionMark, LucideAngularModule, Timer } from 'lucide-angular';
 
 @Component({
   selector: 'app-diploma-details',
-  imports: [Breadcrumbs, PageTitle, BackArrow],
+  imports: [Breadcrumbs, PageTitle, BackArrow, LucideAngularModule],
   templateUrl: './diploma-details.html',
   styleUrl: './diploma-details.css',
 })
 export class DiplomaDetails {
   readonly BookOpenCheck=BookOpenCheck;
+  readonly CircleQuestionMark=CircleQuestionMark;
+  readonly Timer=Timer;
+  exams!: Exam[];
   errorMessage:string='';
   diplomaDetails!:DiplomaDetailsModel;
+  
   private _activatedRoute=inject(ActivatedRoute);
   private  _diplomasService = inject(DiplomasService);
    private _breadcrumbService = inject(BreadcrumbService);
@@ -34,6 +38,9 @@ export class DiplomaDetails {
      this._diplomasService.getDiplomaById(id).subscribe({
         next:(data)=>{
            this.diplomaDetails=data;
+           this.exams=data.exams;
+           
+
            this._breadcrumbService.setCustomBreadcrumb(
               data.title
            );
